edc9ed8c80e490ea99444c875deaedadf259382b
Alejandro Velez-Arce <alejandrovelez-arce@Alejandros-MacBook-Pro.local>
2021-12-12 09:20:43 -0700

    added Pinterest logo

--- src/data/contact.js
+++ src/data/contact.js
@@ -5,6 +5,7 @@ import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
+import { faPinterest } from '@fortawesome/free-brands-svg-icons/faPinterest';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
 
@@ -38,6 +39,11 @@ const data = [
     link: 'https://twitter.com/',
     label: 'Twitter',
     icon: faTwitter,
+  },
+  {
+    link: 'https://www.pinterest.com/avelezarce/',
+    label: 'Pinterest',
+    icon: faPinterest,
   },
   {
     link: 'mailto:alejandro.velez.arce@gmail.com',
