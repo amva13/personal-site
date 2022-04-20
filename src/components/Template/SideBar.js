aa4cf979cd2ff7b2efae647ded75b7abe81a7103
Alejandro Velez-Arce <alejandrovelez-arce@Alejandros-MacBook-Pro.local>
2022-04-20 04:09:07 -0600

    dApp ready

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -21,7 +21,7 @@ const SideBar = () => (
       <h2>About</h2>
       <p>
         Hi, I&apos;m Alex. I&apos;m a product manager working on exciting Web 3.0
-        projects. Outside of work he enjoys many latin dances, international
+        projects. Outside of work I enjoy many latin dances, international
         traveling, exploring new cuisines, obstacle racing, athletics, and
         podcasts through spotify and youtube. Click below to learn more !
       </p>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -12,7 +12,7 @@ const Index = () => (
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
           <p>
-            A really awesome website.
+            A really awesome statically-generated decentralized react application hosted on IPFS.
           </p>
         </div>
       </header>
