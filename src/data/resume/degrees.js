70076418dec44319f9b382c2301ca0098d035847
Alejandro Velez-Arce <alejandrovelez-arce@Alejandros-MacBook-Pro.local>
2022-04-22 00:43:13 -0600

    mend

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,15 @@
 const degrees = [
   {
-    school: 'Northwestern University - Kellogg School of Management',
-    degree: 'Executive Program in Product Management',
-    link: 'https://www.kellogg.northwestern.edu/executive-education/individual-programs/online-programs/pcpm.aspx',
-    year: 2022,
-  },
-  {
     school: 'Massachusetts Institute of Technology',
     degree: 'B.S. Computer Science & Engineering (Course 6-3) ; Minor in Management (Course 15-1 / Sloan) ; Concentration in Economics (Course 14)',
     link: 'https://mit.edu',
     year: 2018,
+  },
+  {
+    school: 'Northwestern University - Kellogg School of Management',
+    degree: 'Executive Program in Product Management',
+    link: 'https://www.kellogg.northwestern.edu/executive-education/individual-programs/online-programs/pcpm.aspx',
+    year: 2022,
   },
   // {
   //   school: 'University at Buffalo',
